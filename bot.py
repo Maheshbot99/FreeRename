93aa8c1790ebec087e9214a6b36fc85b6605f856
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-10-06 08:49:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,38 @@
-from datetime import datetime
-from pytz import timezone
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
-from config import Config
-from aiohttp import web
-from route import web_server
+from pyrogram import Client 
+from config import API_ID, API_HASH, BOT_TOKEN, FORCE_SUB
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
             name="renamer",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=200,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            workers=50,
             plugins={"root": "plugins"},
-            sleep_threshold=15,
+            sleep_threshold=5,
         )
 
     async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            app = web.AppRunner(await web_server())
-            await app.setup()       
-            await web.TCPSite(app, "0.0.0.0", 8080).start()     
-        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
-        for id in Config.ADMIN:
-            try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
-            except: pass
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
-
-Bot().run()
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)                  
+            self.invitelink = link
+         except Exception as e:
+            print(f"{e}")
+            print("Make Sure Bot admin in force sub channel")             
+            self.force_channel = None
+       print(f"{me.first_name} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳 ⚡️⚡️⚡️")
+       
+    async def stop(self, *args):
+      await super().stop()      
+      print("Bot Stopped")
+        
+bot = Bot()
+bot.run()
